28039dfdb9c11782c8230ed792bc7fc6af8fd783
Kelliann Lafferty <kell10192@gmail.com>
2019-06-10 17:09:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,3 +6,14 @@ def my_each(array)
 end 
 array 
 end 
+
+# does not call on .each 
+# does not call on puts 
+# calls on while
+# iterates over each element
+# yields the correct element
+# can handle an empty collection 
+# returned array contains the same elements as the original collection (.each) 
+# does not modify the original collection 
+# block is run n times 
+# only passes a single element into the block at a time 
